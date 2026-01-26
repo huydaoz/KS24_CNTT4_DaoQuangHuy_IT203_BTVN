9209b12c8bfd71e0504e658773a64383fb3eb446
huy dao <zed99hy@gmail.com>
2026-01-27 01:05:26 +0700

    update

--- src/PTIT_CNTT4_IT203_SESSION2/ex5/ex5.java
+++ src/PTIT_CNTT4_IT203_SESSION2/ex5/ex5.java
@@ -6,10 +6,30 @@ public class ex5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int num = 100;
-        int day;
-        while (true){
+        int sum = 100;
 
+        System.out.println("--- HE THONG DANH GIA DOC SACH ---");
+        System.out.println("Nhap so ngay tre. Nhap 999 de ket thuc");
+        while (true){
+            int day;
+            System.out.print("So ngay tra tre cua lan nay: ");
+            day = sc.nextInt();
+            if (day<=0){
+                sum += 5;
+            } else if (day == 999) {
+                break;
+            } else {
+                sum = sum - day*2;
+                System.out.println("-> Tra tre "+day+" ngay: -"+day*2+" diem");
+            }
+        }
+        System.out.println("Tong diem uy tin: " + sum);
+        if (sum>120){
+            System.out.println("Xep loai: Doc gia than thiet");
+        } else if (sum<80) {
+            System.out.println("Xep loai: Doc gia can luu y");
+        }else {
+            System.out.println("Xep loai: Doc gia tieu chuan");
         }
     }
 }
--- src/PTIT_CNTT4_IT203_SESSION2/ex6/ex6.java
+++ src/PTIT_CNTT4_IT203_SESSION2/ex6/ex6.java
@@ -1,4 +1,46 @@
 package PTIT_CNTT4_IT203_SESSION2.ex6;
 
+import java.util.Scanner;
+
 public class ex6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int maxBorrow = -1;
+        int minBorrow = Integer.MAX_VALUE;
+        int totalBorrow = 0;
+        int openDays = 0;
+
+        for (int day = 2; day <= 8; day++) {
+            System.out.print("Nhập số lượt mượn ngày thứ " + day + ": ");
+            int borrow = sc.nextInt();
+
+            if (borrow == 0) {
+                continue; // thư viện đóng cửa, không tính
+            }
+
+            // cập nhật max
+            if (borrow > maxBorrow) {
+                maxBorrow = borrow;
+            }
+
+            // cập nhật min
+            if (borrow < minBorrow) {
+                minBorrow = borrow;
+            }
+
+            totalBorrow += borrow;
+            openDays++;
+        }
+
+        if (openDays > 0) {
+            double average = (double) totalBorrow / openDays;
+
+            System.out.println("Lượt mượn cao nhất: " + maxBorrow);
+            System.out.println("Lượt mượn thấp nhất: " + minBorrow);
+            System.out.println("Lượt mượn trung bình: " + average);
+        } else {
+            System.out.println("Không có ngày nào thư viện mở cửa.");
+        }
+    }
 }
